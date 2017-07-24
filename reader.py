41b387d29c21b0eda26187e7b60b03e963f94bbd
rnarayan7 <roshinan@umich.edu>
2017-07-24 02:37:23 -0700

    readInHEAD function added
    Loads only necessary information into in-memory

--- reader.py
+++ reader.py
@@ -4,13 +4,16 @@ import csv
 class HEAD(object):
     def __init__(self, headings_1, headings_2, description, line_num):
         self.head_description = pd.DataFrame(data = [description], columns = headings_1)
-        self.head_data = pd.DataFrame(columns = headings_2)
         self.data_headings = headings_2
         self.id = int(self.head_description["Head Stack S/N"][0])
         self.line_num = line_num
+        self.data = {}
     def add_row(self,row):
         self.head_data = self.head_data.append(pd.DataFrame([row],columns = self.data_headings),
                                                ignore_index = True)
+    def deleteData(self):
+        self.data = {}
+        
 
 def readInitialCSVFile(file_path):
     master = dict()
@@ -38,6 +41,25 @@ def readInitialCSVFile(file_path):
                     break
             master[new_head.id] = new_head
     return master
+
+def readInHEAD(file_path, headings, line_num):
+    HEAD_data = pd.DataFrame(columns = headings)
+    with open(file_path,'rb') as csvfile:
+        reader = csv.reader(csvfile)        
+        while True:
+            if reader.line_num == line_num:
+                break
+            try:
+                row = next(reader)
+            except StopIteration:
+                print "ERROR: could not find line"
+                break
+        while True:
+            if len(row) == 0:
+                break
+            else:
+                HEAD_data = HEAD_data.append(pd.DataFrame([row],columns = headings),
+                                             ignore_index = True)
 
 if __name__ == '__main__':
     master = readInitialCSVFile("Data-File.csv")
