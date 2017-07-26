07e701f5e729d81c3e2e3eb81d0c3696725b627e
rnarayan7 <roshinan@umich.edu>
2017-07-25 19:22:34 -0700

    Implemented embedded MatPlotLib GUI in middlePanel

--- visualization_interface.py
+++ visualization_interface.py
@@ -1,7 +1,12 @@
 import wx
 import os
+
 from reader import ReadInitialCSVFile
 from reader import ReadInHEADData
+
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 
 # Main window class
 class MainWindow(wx.Frame):
@@ -25,12 +30,12 @@ class MainWindow(wx.Frame):
         leftBox = wx.BoxSizer(wx.VERTICAL)
         middleBox = wx.BoxSizer(wx.VERTICAL)
         # Initialize panels
-        middlePanel = wx.Panel(parent = panel, style = wx.DOUBLE_BORDER)
+        self.middlePanel = wx.Panel(parent = panel, style = wx.DOUBLE_BORDER)
         rightPanel = wx.Panel(parent = panel, style = wx.DOUBLE_BORDER)
         leftPanel = wx.Panel(parent = panel, style = wx.DOUBLE_BORDER)
         # Add components to Main Window BoxSizer
         mainBox.Add(leftPanel, proportion = 1, flag = wx.EXPAND | wx.ALIGN_CENTER | wx.LEFT, border = 10)
-        mainBox.Add(middlePanel, proportion = 3, flag = wx.EXPAND | wx.ALIGN_CENTER | wx.LEFT | wx.RIGHT, border = 5)
+        mainBox.Add(self.middlePanel, proportion = 3, flag = wx.EXPAND | wx.ALIGN_CENTER | wx.LEFT | wx.RIGHT, border = 5)
         mainBox.Add(rightPanel, proportion = 1, flag = wx.EXPAND | wx.ALIGN_CENTER | wx.RIGHT, border = 10)
         panel.SetSizer(mainBox)
         # Create FileDialog button
@@ -39,16 +44,15 @@ class MainWindow(wx.Frame):
         # Create ListBox for HEAD numbers
         self.listBox = wx.ListBox(parent = leftPanel, choices = [], style = wx.LB_SINGLE | wx.LB_ALWAYS_SB)
         self.listBox.Bind(wx.EVT_LISTBOX, self.onSelectHEAD)
-         # Add panels to Left Window BoxSizerBHORA
+         # Add panels to Left Window BoxSizer
         leftBox.Add(openFileDlgBtn, proportion = 0, flag = wx.ALIGN_CENTER | wx.TOP, border = 10)
         leftBox.Add(self.listBox, proportion = 1, flag = wx.EXPAND | wx.ALIGN_LEFT | wx.TOP, border = 10)
         leftPanel.SetSizer(leftBox)
-        # Add Matplot to middle panel
-        self.figure = Figure()
-        self.plt = self.figure.add_subplot(111)
-        canvas = FigureCanvas(middlePanel, -1, self.figure)
-        middleBox.Add(canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
-        middlePanel.SetSizer(middleBox)
+        # Add panels to Middle Window
+        figure = Figure()
+        self.canvas = FigureCanvas(self.middlePanel, -1, figure)
+        middleBox.Add(self.canvas, proportion = 1, flag = wx.EXPAND)
+        self.middlePanel.SetSizer(middleBox)
     
     # Event handler for FileDialog button
     def onOpenFile(self, event):
@@ -96,6 +100,22 @@ class MainWindow(wx.Frame):
         lineNum = self.master[self.currentID].line_num
         headings = self.master[self.currentID].data_headings
         self.master[self.currentID].data = ReadInHEADData(self.currentFilePath, headings, lineNum)
+    
+    # Plots graph based on selected id
+    def PlotGraph(self):
+        currentData = self.master[self.currentID].data
+        if(currentData is None):
+            raise ValueError("data frame empty")
+            return
+        figure = Figure(figsize = (8.5,7))
+        plt = figure.add_axes([0.1,0.1,0.8,0.8])
+        plt.plot(currentData["TS_TFCSetup_AE_Sensor_DEV"],'b')
+        plt.plot(currentData["TS_TFCSetup_AE_Sensor_NMC"],'r')
+        plt.plot(currentData["TS_TFCSetup_AE_Sensor_RMS"],'k')
+        plt.legend()
+        plt.set_xlabel("TFC Power (mW)")
+        plt.set_title(self.master[self.currentID].serial_num)
+        self.canvas = FigureCanvas(self.middlePanel, -1, figure)
 
 # Initialize main
 if __name__ == '__main__':
